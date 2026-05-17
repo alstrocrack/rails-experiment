db22b1c6486755f0ed57b790db624bf6f1686c91
Yuto Urushima <yuto259233@gmail.com>
2026-05-17 18:21:41 +0900

    oss_csv_service

---
+++ app/services/csv_oss_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "aws-sdk-s3"
+require "csv"
+
+class CsvOssService
+  def call(key:)
+    s3_client = Aws::S3::Client.new(region: "ap-northeast-1")
+    object = s3_client.get_object(key: key, bucket: "rails-experiment")
+
+    file_path = Rails.root.join("tmp", "downloaded.csv").to_s
+
+    File.open(file_path, "wb") { |file| file << object.body.read }
+
+    csv = CSV.open(file_path, "r", headers: true)
+    csv.each { |row| puts row }
+  end
+end
